0d4e59b2917118053726beb203b473cc669fe600
Youngha Park <geoffrey.youngha.park@gmail.com>
2025-08-31 16:27:51 +0900

    feat: create first.c

---
+++ chapter02/first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void) // simple program
+{
+	int num; // define variable 'num'
+	num = 1; // assign value to 'num'
+	printf("I am simple "); // using function 'printf()'
+	printf("computer.\n");
+	printf("I want best, so I like number %d.\n",num);
+
+	return 0;
+}
