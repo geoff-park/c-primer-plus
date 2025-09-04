b64f7d9c0bed56da7afda436e291dbe10c8b84c3
Youngha Park <geoffrey.youngha.park@gmail.com>
2025-09-05 00:38:17 +0900

    finish chapter04~ OwO

---
+++ chapter04/input.c
@@ -0,0 +1,16 @@
+// input.c -- & 를 언제 사용하는가
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+	int age; // 변수
+	float assets; // 변수
+	char pet[30]; // 문자열
+
+	printf("나이, 재산, 좋아하는 애완동물을 입력하시오.\n");
+	scanf("%d %f", &age, &assets); // 여기에는 &를 사용한다.
+	scanf("%s", pet); // 문자 배열에는 &를 사용하지 않는다.
+	printf("%d %.2f %s\n", age, assets, pet);
+
+	return 0;
+}
